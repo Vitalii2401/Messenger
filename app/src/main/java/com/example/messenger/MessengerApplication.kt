619552624a866f1fde645b137837cbc3cb03b4f7
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-07-31 15:39:31 +0300

    Added all modules to initialization Koin

--- app/src/main/java/com/example/messenger/MessengerApplication.kt
+++ app/src/main/java/com/example/messenger/MessengerApplication.kt
@@ -1,6 +1,7 @@
 package com.example.messenger
 
 import android.app.Application
+import com.example.messenger.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -17,7 +18,12 @@ class MessengerApplication : Application() {
             androidContext(applicationContext)
             modules(
                 listOf(
-
+                    apiModule,
+                    dataBaseModule,
+                    dataSourceModule,
+                    repositoryModule,
+                    useCaseModule,
+                    viewModelModule
                 )
             )
         }

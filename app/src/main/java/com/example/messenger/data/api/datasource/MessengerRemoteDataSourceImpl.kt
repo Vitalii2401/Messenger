54e0fd5ca94c4e26f135ab4c450d26f410e1a779
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2022-07-31 15:32:28 +0300

    MessengerRemoteDataSourceImpl.kt extends MessengerDataSource.Remote

--- app/src/main/java/com/example/messenger/data/api/datasource/MessengerRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/messenger/data/api/datasource/MessengerRemoteDataSourceImpl.kt
@@ -1,4 +1,6 @@
 package com.example.messenger.data.api.datasource
 
-class MessengerRemoteDataSourceImpl {
+import com.example.messenger.data.datasource.MessengerDataSource
+
+class MessengerRemoteDataSourceImpl : MessengerDataSource.Remote {
 }
